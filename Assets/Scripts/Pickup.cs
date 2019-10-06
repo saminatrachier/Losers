a8c055cb0a65d7a116b85598ada3402fe4da68e4
Samina Trachier <st3225@nyu.edu>
2019-10-06 00:15:51 -0400

    Added Player Movement and Item Pick Up

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Transform player;
+
+    public float throwForce = 10;
+
+    private bool hasPlayer = false;
+
+    private bool beingCarried = false;
+
+    public Rigidbody Rb;
+    
+    private bool Throw = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PileOStuff")
+        {
+            Throw = true;
+        }
+        hasPlayer = true;
+        Debug.Log("Hit!");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PileOStuff")
+        {
+            Throw = false;
+        }
+        hasPlayer = false;
+        Debug.Log("Not Hit!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (beingCarried)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                transform.parent = null;
+                beingCarried = false;
+                Rb.AddForce(player.forward * throwForce);
+                Rb.isKinematic = false;
+
+                Debug.Log("throw");
+
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0) && hasPlayer)
+            {
+                Rb.isKinematic = true;
+                transform.parent = player;
+                beingCarried = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed = 10;
+
+
+
+    Vector3 inputVector;
+
+    private float upDownRotation;
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical"); //corresponds to W/S or Up/Down on a Keyboard, -1 for down +1 for up
+        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right on Keyboard, -1 is left, +1 is right
+		
+        inputVector = transform.forward * vertical * moveSpeed; //forward/back direction
+        inputVector += transform.right * horizontal * moveSpeed; //left/right direction
+    }
+    void FixedUpdate() //all physics code should go in FixedUpdate!!!
+    {
+        //apply our forces to move the object around
+        GetComponent<Rigidbody>().velocity = new Vector3(inputVector.x,GetComponent<Rigidbody>().velocity.y,inputVector.z); //no need for Time.deltatime, already fixed framerate
+
+    }
+    
+
+}
